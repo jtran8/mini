c2048b9102d282318c27693dc160fec60fc994e6
jtran8 <40408767+jtran8@users.noreply.github.com>
2021-09-02 01:34:24 -0400

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import data from '../assets/dataStruct.json'; // Import the JSON from local dir
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Mini Project';
+  badgeData: any = data; // Assign the JSON to the badgeData property
 }
